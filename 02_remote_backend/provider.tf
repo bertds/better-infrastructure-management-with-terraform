dfa2114e54ccaa77fab7878e6c430e89caf51efe
bertds <bert.desutter@gmail.com>
2022-01-31 14:43:17 +0000

    oef 3

--- 02_remote_backend/provider.tf
+++ 02_remote_backend/provider.tf
@@ -8,13 +8,17 @@ terraform {
   }
   backend "s3" {
     // TO DO: configure this correctly after setting up the remote backend
+    bucket = "better-infrastructure-management-with-terraform-72"
+    key = "bert_state" 
+    region  = "eu-west-1"
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region  = "eu-west-1"
+  region  = "eu-west-3"
   profile = "academy"
+
 }
 
 
--- 02_remote_backend/remote_setup/setup.tf
+++ 02_remote_backend/remote_setup/setup.tf
@@ -40,6 +40,10 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
   write_capacity = 1
   hash_key       = "LockID"
 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
   // TO DO: complete this resource block!
 }
 
